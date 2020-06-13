3b03e35fcf7b2337a38b2b72dd201ad3aa77e805
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-13 14:45:16 +0700

    [6. One Quick Change]

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var ID = todo.ID;
+    var title = todo.Title;
+    var finished = todo.finished;
+    console.log("\n    The Todo with ID: " + ID + "  \n    Has a title of: " + title + "\n    Is it finished? " + finished + "\n    ");
 });
