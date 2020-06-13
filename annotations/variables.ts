cf6646be586829a6f89401077e433638c04ef44f
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-13 15:48:40 +0700

    [16. Object Literal Annotations]

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -6,5 +6,19 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 // built in objects;
-
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blud"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
