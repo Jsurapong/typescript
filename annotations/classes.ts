f7aea057f21602a62122f9bfea3232161b89d3db
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-14 16:21:23 +0700

    [45. Instance Method Modifiers]

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,18 +1,23 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+  protected honk(): void {
     console.log("beep");
   }
 }
 
+const vehicle = new Vehicle();
+vehicle.honk();
+
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
-car.drive();
+car.startDrivingProcess();
 car.honk();
