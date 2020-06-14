3a82ed1571865ee098ee84983785ea59d02fb2fa
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-14 11:44:10 +0700

    [Why Typed Arrays?]

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,5 +1,17 @@
-const carMakers = [];
+const carMakers = ["ford", "toyota", "chevy"];
 const dates = [new Date(), new Date()];
 
 // const carsByMake = [["f150"], ["corolla"], ["camaro"]];
 const carsByMake: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
