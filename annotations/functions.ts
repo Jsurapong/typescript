f3f915c383307dc50d167f9589d1e23f6d90b4c7
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-14 11:18:20 +0700

    [25. Annotations for Anonymous Functions]

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -5,3 +5,11 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number): number => {
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
