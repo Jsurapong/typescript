eddb3e8cb2cb663104ae69fa92bf0cd5165b9a71
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-14 16:27:45 +0700

    [46. Fields in Classes]

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,23 +1,26 @@
 class Vehicle {
+  constructor(public color: string) {
+    this.color = color;
+  }
+
   protected honk(): void {
     console.log("beep");
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.honk();
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log("vroom");
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log("vroom");
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+// const car = new Car();
+// car.startDrivingProcess();
