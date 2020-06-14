132bde6b1848ae1b601f69fc03d909428e1d9f72
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-14 16:31:51 +0700

    [47. Fields with Inheritance]

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -11,16 +11,20 @@ class Vehicle {
 const vehicle = new Vehicle("orange");
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log("vroom");
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
 
-//   startDrivingProcess(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+  private drive(): void {
+    console.log("vroom");
+  }
 
-// const car = new Car();
-// car.startDrivingProcess();
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, "red");
+car.startDrivingProcess();
