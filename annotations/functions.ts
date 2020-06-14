2fd613f5ea59dec85306c36a872972f72826d8ea
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-14 11:25:53 +0700

    [27. Destructuring with Annotations]

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -21,3 +21,21 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const forecast = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(forecast);
