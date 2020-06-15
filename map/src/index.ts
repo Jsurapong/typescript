1702223268ec3956d5dac5be9721d1f8b5b7d1a4
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-15 15:09:14 +0700

    [60. Exploring Type Definition Files]

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,8 +1,10 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-
-const company = new Company();
-
-console.log({ user, company });
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
