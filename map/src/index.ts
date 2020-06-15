8528a0980bd87c6fad7c6f43f18bff2c15c7603e
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-15 15:38:51 +0700

    [64. Duplicate Code]

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,7 +1,10 @@
 import { CustomMap } from "./CustomMap";
 import { User } from "./User";
-// import { Company } from "./Company";
+import { Company } from "./Company";
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap("map");
+
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
