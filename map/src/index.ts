ae3b014603d6f90bcf333f4bee5f12598ef78a9d
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-15 15:36:10 +0700

    [63. Adding Markers]

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,10 +1,7 @@
-// import { User } from "./User";
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
 // import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const user = new User();
+const customMap = new CustomMap("map");
+customMap.addUserMarker(user);
