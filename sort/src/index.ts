da26ffcfbc16e69caf6e20dd3f03e59fdf954c74
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-25 15:09:41 +0700

    89. Just...One...More...Fix...

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,3 +1,4 @@
+import { LinkedList } from "./LinkedList";
 import { CharactersCollection } from "./CaractersCollection";
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
@@ -8,8 +9,20 @@ import { NumbersCollection } from "./NumbersCollection";
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("XXaayb");
+// const charactersCollection = new CharactersCollection("XXaayb");
 
-const sorter = new Sorter(charactersCollection);
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+
+linkedList.add(400);
+linkedList.add(100);
+linkedList.add(-999);
+linkedList.add(-888);
+linkedList.add(999);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
