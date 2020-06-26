616a9d4965f6a1691590b0cb15307b62838695da
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-26 14:25:09 +0700

    [101. Running an Analysis]

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -7,4 +7,16 @@ const matches = fs
   .split("\n")
   .map((row: string): string[] => row.split(","));
 
-console.log({ matches });
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins++;
+  }
+
+  if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
