737884cdb63746f0e5b29fe6f68fec414b9c8f43
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-26 15:45:17 +0700

    [115. Applying a Type to a Generic Class]

--- stats/build/generics.js
+++ stats/build/generics.js
@@ -2,9 +2,13 @@
 var HoldAnything = /** @class */ (function () {
     function HoldAnything() {
     }
+    HoldAnything.prototype.add = function (a) {
+        return a;
+    };
     return HoldAnything;
 }());
 var holdNumber = new HoldAnything();
 holdNumber.data = 112;
+holdNumber.add(10);
 var holdString = new HoldAnything();
 holdString.data = "hello";
--- stats/src/generics.ts
+++ stats/src/generics.ts
@@ -1,9 +1,14 @@
 class HoldAnything<TypeOfData> {
   data: TypeOfData;
+
+  add(a: TypeOfData): TypeOfData {
+    return a;
+  }
 }
 
 const holdNumber = new HoldAnything<number>();
 holdNumber.data = 112;
+holdNumber.add(10);
 
 const holdString = new HoldAnything<string>();
 holdString.data = "hello";
