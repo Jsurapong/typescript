6a960c0faa1947eeb87acaeaf70305ed9a62676e
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-26 14:59:42 +0700

    [105. Extracting CSV Reading]

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,25 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default
-    .readFileSync("football.csv", {
-    encoding: "utf-8",
-})
-    .split("\n")
-    .map(function (row) { return row.split(","); });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader("football.csv");
+reader.read();
+var matches = [];
 var manUnitedWins = 0;
-var homeWin = "H";
-var awayWin = "A";
-var draw = "D";
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
-    if (match[1] === "Man United" && match[5] === homeWin) {
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    if (match[2] === "Man United" && match[5] === awayWin) {
+    if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,24 +1,25 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => row.split(","));
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+const matches = [];
 
 let manUnitedWins = 0;
 
-const homeWin = "H";
-const awayWin = "A";
-const draw = "D";
+// enum - enumeration
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === homeWin) {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   }
 
-  if (match[2] === "Man United" && match[5] === awayWin) {
+  if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
