1bc3e522f839a12dbe8b7ea468a4099ca44d3af8
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-26 16:09:42 +0700

    [120. Updating Reader References]

--- stats/build/index.js
+++ stats/build/index.js
@@ -4,9 +4,9 @@ var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
 var CsvFileReader_1 = require("./CsvFileReader");
 // Create an object that satisfies the 'DataReader' interface
+var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
 //Create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' interface
-var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 var manUnitedWins = 0;
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,10 +5,10 @@ import { CsvFileReader } from "./CsvFileReader";
 
 // Create an object that satisfies the 'DataReader' interface
 
+const csvFileReader = new CsvFileReader("football.csv");
+
 //Create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' interface
-
-const csvFileReader = new CsvFileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
